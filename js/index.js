0bd65ddece9415136ad2fa8e31e68d7a7ef57db1
Kenneth Aladi <kennethmichael11@gmail.com>
2020-03-19 13:09:03 +0100

    Worked on the Javascript aspect of the login

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const loginInfo = () => {
+  const userEmail = document.getElementById("email");
+  const userPass = document.getElementById("password");
+  const details = {
+    username: userEmail,
+    password: userPass
+  };
+
+  console.log(details);
+};
